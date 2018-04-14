2b0f5460be01609300a527611737f5dbfdeb8f9e
Victor Larrañaga <victorlgurrea@gmail.com>
2018-04-14 12:58:46 +0200

    desarrollo formulario nuevaTapa

--- src/AppBundle/Controller/GestionTapasController.php
+++ src/AppBundle/Controller/GestionTapasController.php
@@ -6,6 +6,10 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use AppBundle\Entity\Tapa;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 /**
  * @Route("/gestionTapas")
@@ -20,7 +24,13 @@ class GestionTapasController extends Controller
       //Vamos a crear una nueva tapa
       $tapa = new Tapa();
       // le pasamos el objeto tapa al creador de formularios quien con la ayuda de entity Tapa sabe los campos que necesita
+      // creamos el fomulario
       $formBuilder = $this->createFormBuilder($tapa);
+      $formBuilder->add('nombre', TextType::class);
+      $formBuilder->add('descripcion', TextareaType::class);
+      $formBuilder->add('ingredientes', TextareaType::class);
+      $formBuilder->add('fechaCreacion', DateType::class);
+      $formBuilder->add('save', SubmitType::class, array('label' => 'Nueva Tapa'));
       // el formulario se construye con la ayuda de la funcion getForm()
       $form = $formBuilder->getForm();
       // replace this example code with whatever you need
